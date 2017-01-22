e9da7872157d3e59a836f1ffab9eb860872e7333
sin <yasinai@gmail.com>
2017-01-22 13:58:47 +0200

    added check dependency for sequence

--- esxi_mount_nfs.sh
+++ esxi_mount_nfs.sh
@@ -5,23 +5,35 @@ DS_NAME=$DS_NAME
 STORAGENAME=$STORAGENAME
 STARTSEQ=$STARTSEQ
 ENDSEQ=$ENDSEQ
-function MOUNT () {
-if $user_req="MOUNT"
-local ESXSCRIPT='esxcli storage nfs add -H $STORAGENAME -s /$MOUNTNAME -v $DS_NAME'
-echo $ESXSCRIPT
-else 
-local ESXSCRIPT='esxcli storage nfs remove --volume-name=$DS_NAME'
-echo $ESXSCRIPT
-}
+user_req=$user_req
+if [[ "$STARTSEQ" -gt "$ENDSEQ" ]]; then
+	echo "Error Sequence" 
+	exit 0
+fi
 
-$SEQUENCE=`seq $STARTSEQ $ENDSEQ `
-for i in $SEQUENCE;
-do
-expect -c "spawn ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@vmrack${i} $ESXSCRIPT; 
-set timeout 5; 
-expect -nocase -exact \"password: \"; 
-send -- \"$ESXPASSWD\r\"; 
-expect eof" ;
-done
+function MOUNT () {
+if [[ "$user_req" == "MOUNT" ]]
+then
+	local ESXSCRIPT='esxcli storage nfs add -H $STORAGENAME -s /$MOUNTNAME -v $DS_NAME'
+	echo $ESXSCRIPT
+else 
+	local ESXSCRIPT='esxcli storage nfs remove --volume-name=$DS_NAME'
+	echo $ESXSCRIPT
+fi
+}
+ESXSCRIPT=$(MOUNT)
+echo $ESXSCRIPT
+if [[ "$ENDSEQ" == "$STARTSEQ" || "$ENDSEQ" == "" ]];then
+       echo ${SEQUENCE:-$STARTSEQ};
+else
+        SEQUENCE=`seq $STARTSEQ $ENDSEQ `;
+fi
+	for i in $SEQUENCE;
+	do
+	expect -c "spawn ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null root@vmrack${i} $ESXSCRIPT; 
+	set timeout 5; 
+	expect -nocase -exact \"password: \"; 
+	send -- \"$ESXPASSWD\r\"; 
+	expect eof" ;
+	done
 ##"set timeout -1;" with no timeout at all. 
-#echo $ping
